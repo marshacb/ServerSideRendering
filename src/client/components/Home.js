a86d27b3d5bcb3691fe32a5f81e53c73255d6406
Cameron <marshacb@Camerons-MacBook-Pro.local>
2017-10-14 18:09:56 -0400

    initial commit

---
+++ src/client/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+const Home = () => {
+    return (
+    <div>
+        <div>I'm the VERY VERY BEST home component</div>
+        <button onClick={() => console.log("Hi there!")}>Press me!</button>
+    </div>)
+}
+
+export default Home
